97b26cd225fa7a332e229c5f5ad1bbadefeeeef8
navalpablo <navalpablo@hotmail.com>
2023-11-27 09:52:05 +0100

    Added scaffold

--- AddStudy.py
+++ AddStudy.py
@@ -7,7 +7,6 @@ import shutil
 from concurrent.futures import ThreadPoolExecutor
 import multiprocessing
 
-
 def anonymize_dicom_file(input_file, output_file, patient_id):
     try:
         ds = pydicom.dcmread(input_file, force=True)
@@ -30,9 +29,7 @@ def anonymize_dicom_file(input_file, output_file, patient_id):
     except Exception as e:
         print(f"Error processing {input_file}: {e}")
 
-
 def anonymize_wrapper(args):
-    # Unpack arguments
     src_file, dest_file, patient_id = args
     shutil.copy(src_file, dest_file)
     anonymize_dicom_file(dest_file, dest_file, patient_id)
@@ -42,7 +39,7 @@ def process_directory(input_dir, output_dir, patient_id):
         os.makedirs(output_dir)
 
     num_cores = multiprocessing.cpu_count()
-    max_threads = max(1, num_cores - 2)  # Ensuring at least 1 thread
+    max_threads = max(1, num_cores - 2) 
 
     with ThreadPoolExecutor(max_workers=max_threads) as executor:
         tasks = []
@@ -52,25 +49,30 @@ def process_directory(input_dir, output_dir, patient_id):
                 rel_path = os.path.relpath(src_file, input_dir)
                 dest_file = os.path.join(output_dir, rel_path)
                 os.makedirs(os.path.dirname(dest_file), exist_ok=True)
-                # Prepare arguments for the anonymization wrapper
                 args = (src_file, dest_file, patient_id)
-                # Schedule the task
                 tasks.append(executor.submit(anonymize_wrapper, args))
 
-        # Wait for all tasks to complete
         for future in tasks:
             future.result()
-
-
+        print(f"Anonymized files in {os.path.relpath(root, input_dir)}")
 
 def main():
     bidsfolder = os.path.dirname(os.path.realpath(__file__))
+
+    # Set the current working directory to the location of this script
+    os.chdir(bidsfolder)
 
     # Query for subject and session
     subject = input("Enter subject ID (e.g., sub-001): ")
     session = input("Enter session ID (e.g., ses-01): ")
 
-    anon_dicom_folder = os.path.join(bidsfolder, "anondir")
+    # Create BIDS directory structure
+    bidsdir_folder = os.path.join(bidsfolder, "BIDSDIR")
+    print("Creating BIDS directory structure.")
+    scaffold_cmd = ["dcm2bids_scaffold", "-o", bidsdir_folder]
+    subprocess.run(scaffold_cmd)
+
+    anon_dicom_folder = os.path.join(bidsfolder, ".temp_anondir")
     raw_dicom_folder = os.path.join(bidsfolder, "Inbox")
     bidsdir_folder = os.path.join(bidsfolder, "BIDSDIR")
     
@@ -81,24 +83,32 @@ def main():
         sys.exit(1)
 
     anon_sorted_dicom_folder = os.path.join(bidsfolder, "BIDSDIR", "sourcedata", subject)
-    dcm2bids_config = os.path.join(bidsfolder, "BIDSDIR", "code", "dcm2bids_config.json")
+    dcm2bids_config = "dcm2bids_config.json"
+   
 
+    print("Preparing directories for anonymization.")
     os.makedirs(anon_dicom_folder, exist_ok=True)
     os.makedirs(anon_sorted_dicom_folder, exist_ok=True)
 
     # Process directories
+    print("Processing directories for anonymization.")
     process_directory(raw_dicom_folder, anon_dicom_folder, subject)
 
     # Sort and convert DICOM files
+    print("Sorting and converting DICOM files.")
     dicom_sort_cmd = ["dicom_sort", "--copy", anon_dicom_folder, anon_sorted_dicom_folder, "%StudyDate%/%SeriesDescription%"]
     subprocess.run(dicom_sort_cmd)
 
     # Run dcm2bids
-    dcm2bids_cmd = ["dcm2bids", "-d", raw_dicom_folder, "-p", subject, "-s", session, "-c", dcm2bids_config, "-o", bidsdir_folder ]
+    print("Running dcm2bids for NIfTI conversion.")
+    dcm2bids_cmd = ["dcm2bids", "-d", raw_dicom_folder, "-p", subject, "-s", session, "-c", dcm2bids_config, "-o", bidsdir_folder]
     subprocess.run(dcm2bids_cmd)
 
     # Clean up
+    print("Cleaning up temporary files.")
     shutil.rmtree(anon_dicom_folder)
+
+    print("Process completed.")
 
 if __name__ == "__main__":
     main()
